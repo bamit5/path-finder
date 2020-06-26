d46c9843c8d627427cfe7b09482a823f4726542e
Amit Bar <amitbar00@gmail.com>
2020-06-25 22:50:50 -0700

    added other types to node class

--- src/components/Node/Node.tsx
+++ src/components/Node/Node.tsx
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 import './Node.scss';
 
+export const buttonTypes = {
+  ACTIVE: 'active-node',
+  INACTIVE: 'inactive-node',
+  START: 'start-node',
+  END: 'end-node',
+  BRIDGE: 'bridge-node',
+  WALL: 'wall-node'
+}
+const { ACTIVE, INACTIVE, START, END, BRIDGE} = buttonTypes;
+
 interface NodeProps {
-  startingState?: boolean;
+  startingType?: boolean | string;
 }
 
-const Node: React.FC<NodeProps> = ({ startingState = false }) => {
-  const [isActive, setActive] = useState(startingState);
+const Node: React.FC<NodeProps> = ({ startingType }: NodeProps /* TODO this shouldn't be needed?... */) => {
+  if (!startingType) {
+    console.log("YUH");
+    startingType = INACTIVE;
+  } else if (startingType === true) {
+    startingType = ACTIVE;
+  }
+
+  const [type, setType] = useState<string>(startingType);
   return (
     <div
       aria-label="Node"
-      /*draggable={true}*/
-      onClick={() => setActive(!isActive)}
-      className={isActive ? 'active-node' : 'inactive-node'}
+      /* draggable={true} */
+      onClick={() => setType(type !== INACTIVE ? INACTIVE : ACTIVE)}
+      className={type}
     />
   );
 };
